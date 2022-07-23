c85adaec1804711a093e494fa64f022fc6c28963
erildojs <patriciopatrick92@gmail.com>
2022-07-24 00:25:01 +0100

    add global style

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { GlobalStyle } from "./styles/global";
+
+export function App() {
+  return (
+    <div className="App">
+     <GlobalStyle />
+    </div>
+  );
+}
+
+//svg - melhor padrao para web
+//css scoped - 
+//diminuindo o css
+//16px vezes 0,9375 = 15px
+//16px vezes 0,875 = 14px
+
