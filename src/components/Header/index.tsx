30fe7dc52ae57951a0950b7f30b653bab36aad4f
Erildojs <patriciopatrick92@gmail.com>
2022-07-27 15:35:01 +0100

    remove chakraui styles and add styled components

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,14 +1,15 @@
 import logoImg from '../../assets/logo.svg'
-import {Box, Flex, Button} from '@chakra-ui/react'
-
+//import {Box, Flex, Button} from '@chakra-ui/react'
+import {Container, Content} from './styles'
 export function Header() {
   return (
-    <Box bg='#5429cc'>
-      <Flex maxW={1120} my={0} mx={'auto'} pt={8} pb={48} pr={4} align="center" justify="space-between">
-
+    <Container>
+      <Content>
       <img src={logoImg} alt="dt money" />
-      <Button type="button" cursor="pointer" fontSize={16} color='#FFF' bg='#6933ff' border={0} py={0} px={8} borderRadius={6} h={12} _hover={{'bg': '#6c45d9'}} transition='ease-in'>Nova Transaçaõ</Button>
-      </Flex>
-    </Box>
+      <button type='button'>
+        Nova Transação
+      </button>
+      </Content>
+    </Container>
   )
 }
