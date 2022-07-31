6df648c3c0b7124f0de5c85d22f268cea5fe3d31
Erildojs <patriciopatrick92@gmail.com>
2022-07-31 14:54:12 +0100

    chore: add miragejs and cofigure routes

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,26 @@ import { GlobalStyle } from "./styles/global";
 import {ChakraProvider} from '@chakra-ui/react'
 import { Header } from "./components/Header";
 import { Dashboard } from "./components/Dashboard";
+import {createServer} from 'miragejs'
 
+createServer({
+  routes() {
+    this.namespace = 'api'
+
+    this.get('/transactions', () => {
+      return [
+        {
+          id: 1,
+          title: 'Transaction 1',
+          amount: 400,
+          type: 'deposit',
+          category: 'Food',
+          createdAt: new Date(),
+        }
+      ]
+    })
+  }
+})
 export function App() {
 
   return (
