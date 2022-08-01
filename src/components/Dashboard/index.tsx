3f8ab45025087a9acee5d65ed076054f5eb4754a
Erildojs <patriciopatrick92@gmail.com>
2022-08-01 15:02:42 +0100

    chore: add react modal

--- src/components/Dashboard/index.tsx
+++ src/components/Dashboard/index.tsx
@@ -1,8 +1,14 @@
+import { useEffect } from "react";
+import { api } from "../../services/api";
 import { Summary } from "../Summary";
 import { TransactionsTable } from "../TransactionsTable";
 import { Container } from "./styles";
 
 export function Dashboard() {
+    useEffect(() => {
+        api.get('transactions')
+        .then(response => console.log(response.data))
+    }, [])
     return (
         <Container>
             <Summary />
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,14 +1,30 @@
+import { useState } from 'react'
+import ReactModal from 'react-modal'
 import logoImg from '../../assets/logo.svg'
 //import {Box, Flex, Button} from '@chakra-ui/react'
 import {Container, Content} from './styles'
 export function Header() {
+  const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(false)
+
+  function handleOpenNewTransactionsModal() {
+    setIsNewTransactionModalOpen(true)
+  }
+
+  function handleCloseNewTransactionsModal() {
+    setIsNewTransactionModalOpen(false)
+  }
+
   return (
     <Container>
       <Content>
       <img src={logoImg} alt="dt money" />
-      <button type='button'>
+      <button type='button' onClick={handleOpenNewTransactionsModal}>
         Nova Transação
       </button>
+
+      <ReactModal isOpen={isNewTransactionModalOpen} onRequestClose={handleCloseNewTransactionsModal}>
+        <h2>Cadastrar nova Transaçao</h2>
+      </ReactModal>
       </Content>
     </Container>
   )
