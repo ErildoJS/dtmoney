78047155115dfcefba448b3fa2d14a52bbbe87fc
Erildojs <patriciopatrick92@gmail.com>
2022-08-06 14:03:43 +0100

    feat: list all transactions

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,50 @@ import { GlobalStyle } from "./styles/global";
 import {ChakraProvider} from '@chakra-ui/react'
 import { Header } from "./components/Header";
 import { Dashboard } from "./components/Dashboard";
-import {createServer} from 'miragejs'
+import {createServer, Model} from 'miragejs'
 import Modal from "react-modal";
 import { useState } from "react";
 import { NewTransactionModal } from "./components/NewTransactionModal";
 
 createServer({
+  models: {
+    transaction: Model
+  },
+
+  seeds(server) {
+    server.db.loadData({
+      transactions: [
+        {
+          id: 1,
+          title: 'Pagar renda',
+          type: 'withdraw',
+          category: 'casa',
+          amount: 4000,
+          createdAt: new Date('2022-08-06 12:00:00'),
+        },
+        {
+          id: 2,
+          title: 'Propina da faculdade',
+          type: 'withdraw',
+          category: 'faculdade',
+          amount: 450,
+          createdAt: new Date('2022-08-06 12:30:00'),
+        },
+        {
+          id: 3,
+          title: 'Criando um website',
+          type: 'deposit',
+          category: 'tecnologia',
+          amount: 1400,
+          createdAt: new Date('2022-08-06 11:00:00'),
+        }
+      ]
+    })
+  },
   routes() {
     this.namespace = 'api'
 
-    this.get('/transactions', () => {
+    /*this.get('/transactions', () => {
       return [
         {
           id: 1,
@@ -22,11 +56,14 @@ createServer({
           createdAt: new Date(),
         }
       ]
-    })
+    })*/
 
+    this.get('/transactions', () => {
+      return this.schema.all('transaction')
+    })
     this.post('/transactions', (schema, request) => {
-      const data = request.requestBody
-      return data
+      const data = JSON.parse(request.requestBody)
+      return schema.create('transaction', data)
     })
   }
 })
