6ac1667dd16e11bebd3b6c43c1a5c4d69feff29b
Erildojs <patriciopatrick92@gmail.com>
2022-08-08 11:51:15 +0100

    add context api

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { GlobalStyle } from "./styles/global";
-import {ChakraProvider} from '@chakra-ui/react'
+//import {ChakraProvider} from '@chakra-ui/react'
 import { Header } from "./components/Header";
 import { Dashboard } from "./components/Dashboard";
 import {createServer, Model} from 'miragejs'
 import Modal from "react-modal";
 import { useState } from "react";
 import { NewTransactionModal } from "./components/NewTransactionModal";
+import {TransactionsContext} from './TransactionsContext'
+
 
 createServer({
   models: {
@@ -82,14 +84,14 @@ export function App() {
   }
 
   return (
-    <ChakraProvider>
+    <TransactionsContext.Provider value={[]}>
       <Header onpenNewTransactionsModal={handleOpenNewTransactionsModal} />
       <Dashboard />
 
       <NewTransactionModal isOpen={isNewTransactionModalOpen} onRequestClose={handleCloseNewTransactionsModal}/>
 
       <GlobalStyle />
-    </ChakraProvider>
+    </TransactionsContext.Provider>
   );
 }
 
@@ -98,4 +100,6 @@ export function App() {
 //diminuindo o css
 //16px vezes 0,9375 = 15px
 //16px vezes 0,875 = 14px
+//prop drilin
+
 
